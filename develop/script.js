30bb279d78ffca1c87e824e78aa6d6b655381b40
Trill <ty3torr3s@icloud.com>
2021-09-26 11:31:14 -0400

    updated javascript & current date & time section

--- develop/script.js
+++ develop/script.js
@@ -13,3 +13,22 @@ function renderClock() {
 }
 
 setInterval(renderClock, 1000);
+
+let addTimeBlock = "<div></div>";
+for (let i = 1; i <= 24; i++) {
+    let addHour = `${i} am`;
+    if (i == 12) {
+        addHour = `${i} pm`;
+    }
+    if (i > 12) {
+        addHour = `${i - 12} pm`;
+        if (i === 24) {
+            addHour = "12 am";
+        }
+    }
+
+
+
+
+    
+}
