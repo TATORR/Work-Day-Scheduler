32066031e59d4f5a8b02a9c3f1dd3b716d04dee7
Trill <ty3torr3s@icloud.com>
2021-09-26 11:56:45 -0400

    troubleshooted javascript time

--- develop/script.js
+++ develop/script.js
@@ -27,15 +27,15 @@ for (let i = 1; i <= 24; i++) {
         }
     }
 
-    addTimeBlock += <div class="row-time-block">
-    <div class="col-2 hourCol" id="${i}"> ${addHour}</div>
-    <div class="col-9 textCol" id="input-${i}"> <textarea id="input-${i}-text" style = "width: 100%; border-left: 0px !important;"></textarea></div>
-    <div class ="col-1 saveBtn" id ="saveBtn-${i}" data-hour="${i}"> <i class = " fa-lock"></i></div>
-    </div>
+    addTimeBlock += `<div class="row time-block"> 
+    <div class="col-2 hourCol" id="${i}">${addHour}</div>
+    <div class="col-9 textCol" id="input-${i}"><textarea id="input-${i}-text" style="width: 100%; border-left: 0px !important;"></textarea></div>
+    <div class="col-1 saveBtn" id="saveBtn-${i}" data-hour="${i}"><i class="fa fa-lock"></i></div>
+    </div>`;
 }
 
 $("#timeBlockContainer").append(addTimeBlock);
+
 updateTimeBlocks();
 getFromLocalStorage();
-
 //color coding //
